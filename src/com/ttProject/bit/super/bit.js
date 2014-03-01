dca8112a64bf6842415836049604cd98c478d6b7
taktod <poepoemix@hotmail.com>
2014-03-01 20:06:41 +0900

    これなら、干渉されないみたい。

--- src/com/ttProject/bit/super/bit.js
+++ src/com/ttProject/bit/super/bit.js
@@ -4,44 +4,44 @@ goog.provide("com.ttProject.bit.super.Bit");
  * bitデータの基本クラス
  */
 (function(path) {
-	var value = 0;
-	var bitCount = 0;
 	/**
 	 * @constructor
 	 */
 	path.Bit = function(count) {
-		bitCount = count;
+		this.bitCount = count;
 	};
+	path.Bit.prototype.value = 0;
+	path.Bit.prototype.bitCount = 0;
 	/**
 	 * データ設定
 	 * @param value
 	 */
 	path.Bit.prototype.set = function(val) {
-		value = val;
+		this.value = val;
 	};
 	/**
 	 * データ参照
 	 * @returns {Number}
 	 */
 	path.Bit.prototype.get = function() {
-		return value;
+		return this.value;
 	};
 	/**
 	 * データの長さ参照
 	 * @returns {Number}
 	 */
 	path.Bit.prototype.getBitCount = function() {
-		return bitCount;
+		return this.bitCount;
 	};
 	/**
 	 * データの長さ設定
 	 * @returns {Number}
 	 */
 	path.Bit.prototype.setBitCount = function(val) {
-		bitCount = val;
+		this.bitCount = val;
 	};
 	path.Bit.prototype.dump = function() {
-		console.log("value:" + value);
-		console.log("bitCount:" + bitCount);
+		console.log("value:" + this.value);
+		console.log("bitCount:" + this.bitCount);
 	};
 })(com.ttProject.bit.super);

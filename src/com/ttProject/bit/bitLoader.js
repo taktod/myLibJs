91023f84983c5241507b91c855ec4511d92474e4
taktod <poepoemix@hotmail.com>
2014-03-02 21:47:00 +0900

    チャンネルというオブジェクトをつくって処理するか・・・それともuint8Arrayからつくれるようにするか・・・ どうするかねぇ・・・

---
+++ src/com/ttProject/bit/bitLoader.js
@@ -0,0 +1,14 @@
+goog.provide("com.ttProject.bit.BitLoader");
+
+
+
+/**
+ * bitデータの読み込み補助
+ * uint8Arrayからbitデータを順番に読み出します。
+ * もしくは、channelをつくってそちらから読み出すか・・・
+ */
+(function(path) {
+	path.BitLoader = function() {
+		
+	};
+})(com.ttProject.bit);

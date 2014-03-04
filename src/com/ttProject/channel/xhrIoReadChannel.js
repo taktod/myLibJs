4792d6a304d140f29b7bad664b426238df55e433
taktod <poepoemix@hotmail.com>
2014-03-04 23:31:08 +0900

    とりあえずそれっぽいのができてきたが・・・

--- src/com/ttProject/channel/xhrIoReadChannel.js
+++ src/com/ttProject/channel/xhrIoReadChannel.js
@@ -11,41 +11,97 @@ goog.require("com.ttProject.util.HexUtil");
  * http://ja.wikipedia.org/wiki/XMLHttpRequest#.E3.82.B9.E3.83.88.E3.83.AA.E3.83.BC.E3.83.9F.E3.83.B3.E3.82.B0
  * とりあえず、onReadyStateChangeをみて、追加データをとればいい感じかな・・・
  * readするときにデータが足りなければちょくちょく、setIntervalかなにかかけて、待たせる感じで・・・
- * この書き方はjavascriptとしては気持ち悪いだろうね。
  * javascriptにはsleepがないので、まっといて・・・というのができないっぽいですね。
+ * 
  * @param path
  */
 (function(path){
-	path.XhrIoReadChannel = function() {
+	path.XhrIoReadChannel = function(targetUrl) {
 		// byte -> stringの変換方法メモ
 		// String.fromCharCode.apply(null, new Uint8Array([0x30,0x31,0x32,0x33]));
+		var _this = this;
 		// とりあえず動作テストしてみる。
-		xhr = new goog.net.XhrIo();
+		var xhr = new goog.net.XhrIo();
+		// 応答はbinaryとして受け取る
 		xhr.setResponseType(goog.net.XhrIo.ResponseType.ARRAY_BUFFER);
-		xhr.headers.set("Range", "bytes=0-10"); // rangeリクエストはいけるっぽい。
-		goog.events.listen(xhr, goog.net.EventType.READY_STATE_CHANGE, function(e) {
+		// とりあえず、先頭の12バイトだけ読み込む
+		xhr.headers.set("Range", "bytes=0-12"); // rangeリクエストはいけるっぽい。
+		// readyStateChangeはつかえないっぽい。
+/*		goog.events.listen(xhr, goog.net.EventType.READY_STATE_CHANGE, function(e) {
 			console.log(e.target.getResponse());
-		});
+		});*/
 		goog.events.listen(xhr, goog.net.EventType.COMPLETE, function(e) {
 			// 応答データはここでしか拾えないみたい。
+			_this.size = /\/(\d+)/.exec(e.target.getResponseHeader("Content-Range"))[1]; // 相手のデータのサイズを取得できる。
+			_this.accessFlg = true;
+			console.log(e.target);
 			console.log(e.target.getResponse());
 			console.log(com.ttProject.util.HexUtil.toHex(new Uint8Array(e.target.getResponse())));
 		});
-		xhr.send("../ahiru.flv");
+		this.pos = 0;
+		this.accessFlg = false;
+		this.url = targetUrl;
+		xhr.send(targetUrl);
 	};
 	goog.inherits(path.XhrIoReadChannel, path.IReadChannel);
+	// 対象url
+	path.XhrIoReadChannel.prototype.url = null;
+	// 対象データのデータ量
+	path.XhrIoReadChannel.prototype.size = null;
+	// 処理位置
+	path.XhrIoReadChannel.prototype.pos = null;
+	path.XhrIoReadChannel.prototype.accessFlg = null;
 	path.XhrIoReadChannel.prototype.isOpen = function() {
+		// とりあえず開いているフラグは意味をなさないと思う
+		return this.accessFlg;
 	};
 	path.XhrIoReadChannel.prototype.size = function() {
-		
+		if(!this.accessFlg) {
+			throw new Error("接続が破棄済みです。");
+		}
+		// はじめにちょっとだけデータをDLして相手のデータサイズを知っておきたい
+		return this.size;
 	};
 	path.XhrIoReadChannel.prototype.position = function(position) {
-		
+		if(!this.accessFlg) {
+			throw new Error("接続が破棄済みです。");
+		}
+		// 処理位置を保持しておく必要ありそう。
+		if(position == undefined) {
+			return this.pos;
+		}
+		else {
+			if(position > this.size) {
+				throw new Error("コンテンツデータより大きな値が指定されました。");
+			}
+			this.pos = position;
+		}
+		return this;
 	};
 	path.XhrIoReadChannel.prototype.read = function(uint8Array, callback) {
-		
+		if(!this.accessFlg) {
+			throw new Error("接続が破棄済みです。");
+		}
+		// xhrIoで配列分だけデータを読みこんでcallbackで応答を返します。
+		var _this = this;
+		var xhr = new goog.net.XhrIo();
+		// 応答はbinaryとして受け取る
+		xhr.setResponseType(goog.net.XhrIo.ResponseType.ARRAY_BUFFER);
+		// とりあえず、先頭の12バイトだけ読み込む
+		console.log("bytes=" + this.pos + "-" + (this.pos + uint8Array.length));
+		xhr.headers.set("Range", "bytes=" + this.pos + "-" + (this.pos + uint8Array.length));
+		goog.events.listen(xhr, goog.net.EventType.COMPLETE, function(e) {
+			_this.pos += uint8Array.length;
+			var responseArray = new Uint8Array(e.target.getResponse());
+			callback(responseArray);
+		});
+		xhr.send(this.url);
 	};
 	path.XhrIoReadChannel.prototype.close = function() {
-		
+		if(!this.accessFlg) {
+			throw new Error("接続が破棄済みです。");
+		}
+		// これ以上アクセスしないようにします。
+		this.accessFlg = false;
 	};
 })(com.ttProject.channel);

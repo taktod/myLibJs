97f6af33101c3d63ff75aa93e2bc7be0df730931
taktod <poepoemix@hotmail.com>
2014-03-09 23:10:50 +0900

    クラス名変更。

---
+++ src/com/ttProject/frame/mp3/mp3FrameAnalyzer.js
@@ -0,0 +1,31 @@
+goog.provide("com.ttProject.frame.mp3.Mp3FrameAnalyzer");
+
+goog.require("com.ttProject.frame.super.IAnalyzer");
+goog.require("com.ttProject.frame.mp3.Mp3FrameSelector");
+goog.require("com.ttProject.frame.mp3.Mp3Frame");
+
+/**
+ * @constructor
+ */
+com.ttProject.frame.mp3.Mp3FrameAnalyzer = function() {
+	this._selector = new com.ttProject.frame.mp3.Mp3FrameSelector();
+};
+
+goog.inherits(com.ttProject.frame.mp3.Mp3FrameAnalyzer, com.ttProject.frame.super.IAnalyzer);
+
+/**
+ * フレームを解析して取り出す
+ * @param channel
+ */
+com.ttProject.frame.mp3.Mp3FrameAnalyzer.prototype.analyzer = function(channel, callback) {
+	this._selector.select(channel, function(frame){
+		if(frame instanceof com.ttProject.frame.mp3.Mp3Frame) {
+			frame.load(channel, function() {
+				callback(frame);
+			});
+		}
+		else {
+			callback(frame);
+		}
+	});
+};
---
+++ src/com/ttProject/frame/mp3/mp3FrameSelector.js
@@ -0,0 +1,12 @@
+goog.provide("com.ttProject.frame.mp3.Mp3FrameSelector");
+
+/**
+ * @constructor
+ */
+com.ttProject.frame.mp3.Mp3FrameSelector = function() {
+	
+};
+
+com.ttProject.frame.mp3.Mp3FrameSelector.prototype.select = function(channel, callback) {
+	
+};

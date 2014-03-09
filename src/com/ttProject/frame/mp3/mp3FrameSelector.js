9fccaf000638e09c02f293fa0c8448e86298d810
taktod <poepoemix@hotmail.com>
2014-03-09 23:21:01 +0900

    update

--- src/com/ttProject/frame/mp3/mp3FrameSelector.js
+++ src/com/ttProject/frame/mp3/mp3FrameSelector.js
@@ -1,5 +1,8 @@
 goog.provide("com.ttProject.frame.mp3.Mp3FrameSelector");
 
+goog.require("com.ttProject.bit.Bit8");
+goog.require("com.ttProject.bit.BitLoader");
+
 /**
  * @constructor
  */
@@ -8,5 +11,11 @@ com.ttProject.frame.mp3.Mp3FrameSelector = function() {
 };
 
 com.ttProject.frame.mp3.Mp3FrameSelector.prototype.select = function(channel, callback) {
-	
+	// まず始めの8bitを読み込んでみる。
+	var firstByte = new com.ttProject.bit.Bit8();
+	var loader = new com.ttProject.bit.BitLoader(channel);
+	loader.load(firstByte, function() {
+		// 読み込みがおわったときの処理
+		// firstByteからframeTypeを選択
+	});
 };

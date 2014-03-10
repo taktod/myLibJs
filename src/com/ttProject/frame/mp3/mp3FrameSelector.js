d6d0b16a8d785788536893ff1f2674d3464007d7
taktod <poepoemix@hotmail.com>
2014-03-10 22:12:21 +0900

    mp3の読み込みの作成をちょっと進めた。

--- src/com/ttProject/frame/mp3/mp3FrameSelector.js
+++ src/com/ttProject/frame/mp3/mp3FrameSelector.js
@@ -2,6 +2,7 @@ goog.provide("com.ttProject.frame.mp3.Mp3FrameSelector");
 
 goog.require("com.ttProject.bit.Bit8");
 goog.require("com.ttProject.bit.BitLoader");
+goog.require("com.ttProject.frame.mp3.type.Id3Frame");
 
 /**
  * @constructor
@@ -17,5 +18,23 @@ com.ttProject.frame.mp3.Mp3FrameSelector.prototype.select = function(channel, ca
 	loader.load(firstByte, function() {
 		// 読み込みがおわったときの処理
 		// firstByteからframeTypeを選択
+		var frame = null;
+		switch(firstByte.get()) {
+		case 0x49: // 'I'
+			console.log("ID3Frame");
+			frame = new com.ttProject.frame.mp3.type.Id3Frame();
+			break;
+		case 0x54: // 'T'
+//			console.log("Tag");
+//			break;
+			throw new Error("Tagの動作は未実装です");
+		case 0xFF: // frame
+			console.log("frame");
+			break;
+		default:
+			throw new Error("解析不能なデータです");
+		}
+		console.log(frame);
+		frame.minimumLoad(channel, callback);
 	});
 };
--- src/com/ttProject/frame/mp3/type/id3Frame.js
+++ src/com/ttProject/frame/mp3/type/id3Frame.js
@@ -11,6 +11,7 @@ goog.require("com.ttProject.bit.Bit7");
  * @constructor
  */
 com.ttProject.frame.mp3.type.Id3Frame = function() {
+	console.log("id3Frame");
 	this._signature = new com.ttProject.bit.Bit24();
 	this._version   = new com.ttProject.bit.Bit16();
 	this._flag      = new com.ttProject.bit.Bit8();
@@ -25,5 +26,33 @@ com.ttProject.frame.mp3.type.Id3Frame = function() {
 	this._data;
 };
 
-goog.inherit(com.ttProject.frame.mp3.type.Id3Frame, com.ttProject.frame.mp3.Mp3Frame);
+goog.inherits(com.ttProject.frame.mp3.type.Id3Frame, com.ttProject.frame.mp3.Mp3Frame);
 
+/**
+ * 最低限ロード
+ * @param channel
+ * @param callback
+ */
+com.ttProject.frame.mp3.type.Id3Frame.prototype.minimumLoad = function(channel, callback) {
+	var _this = this;
+	var signature = new com.ttProject.bit.Bit16();
+	var loader = new com.ttProject.bit.BitLoader(channel);
+	loader.load(signature, this._version, this._flag,
+			this._dummy1, this._size1, this._dummy2, this._size2,
+			this._dummy3, this._size3, this._dummy4, this._size4,
+			function() {
+		_this._signature.set(0x49 << 16 | signature.get());
+		// このデータのサイズは
+		console.log(10 + (_this._size1.get() << 21 | _this._size2.get() << 14 | _this._size3.get() << 7 | _this._size4.get()));
+		callback(_this);
+	});
+};
+
+/**
+ * detailロード
+ * @param channel
+ * @param callback
+ */
+com.ttProject.frame.mp3.type.Id3Frame.prototype.load = function(channel, callback) {
+	
+};

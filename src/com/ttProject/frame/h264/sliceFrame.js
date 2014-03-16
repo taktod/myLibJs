bfc5cd884895c84556170fb119fdc4036900bae4
taktod <poepoemix@hotmail.com>
2014-03-16 13:22:48 +0900

    作成途上
    先にflvのコンテナ動作あたりつくった方がいいかも・・・テストできない。

--- src/com/ttProject/frame/h264/sliceFrame.js
+++ src/com/ttProject/frame/h264/sliceFrame.js
@@ -4,12 +4,15 @@ goog.require("com.ttProject.frame.h264.H264Frame");
 goog.require("com.ttProject.bit.BitLoader");
 goog.require("com.ttProject.bit.Ueg");
 
+/**
+ * @constructor
+ */
 com.ttProject.frame.h264.SliceFrame = function(forbiddenZeroBit, nalRefIdc, type) {
 	goog.base(this, forbiddenZeroBit, nalRefIdc, type);
 	this._firstMbInSlice        = new com.ttProject.bit.Ueg();
 	this._sliceType             = new com.ttProject.bit.Ueg();
 	this._pictureParameterSetId = new com.ttProject.bit.Ueg();
-	this._extraBit              = null;;
+	this._extraBit              = null;
 };
 
 goog.inherits(com.ttProject.frame.h264.SliceFrame, com.ttProject.frame.h264.H264Frame);
--- src/com/ttProject/frame/h264/type/slice.js
+++ src/com/ttProject/frame/h264/type/slice.js
@@ -1,1 +1,19 @@
 goog.provide("com.ttProject.frame.h264.type.Slice");
+
+goog.require("com.ttProject.frame.h264.SliceFrame");
+
+/**
+ * @constructor
+ */
+com.ttProject.frame.h264.type.Slice = function(forbiddenZeroBit, nalRefIdc, type) {
+	goog.base(this, forbiddenZeroBit, nalRefIdc, type);
+};
+
+goog.inherits(com.ttProject.frame.h264.type.Slice, com.ttProject.frame.h264.SliceFrame);
+
+com.ttProject.frame.h264.type.Slice.prototype.minimumLoad = function(channel, callback) {
+	goog.base(this, "minimumLoad", channel, function() {
+		// ここきた？
+		callback();
+	});
+};

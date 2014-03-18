72c8ead35f8bbb8a59db0c03668ee2b5834421b4
taktod <poepoemix@hotmail.com>
2014-03-18 23:56:35 +0900

    adtsUnitの動作はできあがりだと思う。 あとは、aacの動作が完全にできていないので、調整しないとだめ。

--- src/com/ttProject/container/adts/adtsUnitWriter.js
+++ src/com/ttProject/container/adts/adtsUnitWriter.js
@@ -1,8 +1,25 @@
 goog.provide("com.ttProject.container.adts.AdtsUnitWriter");
 
+goog.require("com.ttProject.container.super.Writer");
+
 /**
  * @constructor
  */
 com.ttProject.container.adts.AdtsUnitWriter = function() {
-	
+	goog.base(this);
+	this._frameList = [];
+};
+
+goog.inherits(com.ttProject.container.adts.AdtsUnitWriter, com.ttProject.container.super.Writer);
+
+com.ttProject.container.adts.AdtsUnitWriter.prototype.addContainer = function(adtsUnit) {
+	this.addFrame(adtsUnit.getFrame());
+};
+
+com.ttProject.container.adts.AdtsUnitWriter.prototype.addFrame = function(aacFrame) {
+	this._frameList.push(aacFrame.getData());
+};
+
+com.ttProject.container.adts.AdtsUnitWriter.prototype.getBlob = function() {
+	return new Blob(this._frameList, {type: "audio/adts"});
 };

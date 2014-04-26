94e4c752d37d6bf4de41c95cac2e7579af13e7b6
taktod <poepoemix@hotmail.com>
2014-04-26 18:36:27 +0900

    基本データ用の動作テストをすべてtest.jsにもっていった。

--- test/test.js
+++ test/test.js
@@ -3,6 +3,7 @@ goog.provide("com.ttProject.Test");
 goog.require("com.ttProject.util.StackUtil");
 goog.require("com.ttProject.util.HexUtil");
 goog.require("com.ttProject.channel.ReadChannel");
+goog.require("com.ttProject.channel.Uint8ReadChannel");
 goog.require("com.ttProject.bit.BitLoader");
 goog.require("com.ttProject.bit.BitConnector");
 goog.require("com.ttProject.bit.Bit1");
@@ -13,6 +14,8 @@ goog.require("com.ttProject.bit.Bit5");
 goog.require("com.ttProject.bit.Bit6");
 goog.require("com.ttProject.bit.Bit7");
 goog.require("com.ttProject.bit.Bit8");
+goog.require("com.ttProject.bit.Ueg");
+goog.require("com.ttProject.bit.Seg");
 goog.require("com.ttProject.bit.EbmlValue");
 
 /**
@@ -29,7 +32,7 @@ function testStack() {
 		});
 	};
 	func();
-}
+};
 
 /**
  * hexUtilの動作について確認
@@ -56,11 +59,48 @@ function testBit() {
 	if(val != "CF 11 ") {
 		throw new Error("bit動作出力が意図しない値になりました。");
 	}
-}
+};
 
+/**
+ * ebmlValueの読み込み動作テスト
+ */
+function testLoadEbmlValue() {
+	var channel = new com.ttProject.channel.Uint8ReadChannel(com.ttProject.util.HexUtil.makeBuffer("814005"));
+	var ebml1 = new com.ttProject.bit.EbmlValue();
+	var ebml2 = new com.ttProject.bit.EbmlValue();
+	var loader = new com.ttProject.bit.BitLoader(channel);
+	loader.load(ebml1, ebml2, function() {
+		console.log(ebml1.get());
+		console.log(ebml2.get());
+		if(ebml1.get() != 1 || ebml2.get() != 5) {
+			throw new Error("ebmlの値が想定外でした。");
+		}
+	});
+};
+
+/**
+ * expGolombの動作テスト
+ */
+function testLoadGolomb() {
+	var channel = new com.ttProject.channel.Uint8ReadChannel(com.ttProject.util.HexUtil.makeBuffer("68"));
+	var ueg = new com.ttProject.bit.Ueg();
+	var seg = new com.ttProject.bit.Seg();
+	var loader = new com.ttProject.bit.BitLoader(channel);
+	loader.load(ueg, seg, function() {
+		console.log(ueg.get());
+		console.log(seg.get());
+		if(ueg.get() != 2 || seg.get() != 1) {
+			throw new Error("golombのデータの読み込み値がおかしいです。");
+		}
+	});
+};
+
+/**
+ * ファイルの読み込み動作テスト
+ */
 function testLoadFile() {
 	var channel = new com.ttProject.channel.ReadChannel("test.dat");
 	channel.read(4, function(data) {
 		console.log(com.ttProject.util.HexUtil.toHex(data, true));
 	});
-}
+};
